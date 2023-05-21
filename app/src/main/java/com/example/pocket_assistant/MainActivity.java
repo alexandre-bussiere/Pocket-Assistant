2116e41a0a598e958bd02597da851c4d5e9162cb
aldesse <Bussiere.alexandre@gmail.com>
2023-05-21 21:10:38 +0200

    Je peux trac plusieur valeur de gyro

--- app/src/main/java/com/example/pocket_assistant/MainActivity.java
+++ app/src/main/java/com/example/pocket_assistant/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.pocket_assistant;
 
-import android.os.Bundle;
+import static android.hardware.Sensor.TYPE_GYROSCOPE;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -12,9 +19,20 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.pocket_assistant.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private ActivityMainBinding binding;
+    private TextView textView2 = null;
+    private TextView textView3 = null;
+    private TextView textView4 = null;
+    private TextView textView5 = null;
+    private TextView textView6 = null;
+    private Button btnUpdate;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private Sensor mGyroscope;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +41,15 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        textView2 = (TextView) findViewById(R.id.textView2);
+        textView3 = (TextView) findViewById(R.id.textView3);
+        textView4 = (TextView) findViewById(R.id.textView4);
+        textView5 = (TextView) findViewById(R.id.textView5);
+        textView6 = (TextView) findViewById(R.id.textView6);
+        btnUpdate = (Button) findViewById(R.id.button);
+
+        SensorActivity();
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
@@ -33,5 +57,35 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
+     public void SensorActivity() {
+         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+         mGyroscope = mSensorManager.getDefaultSensor(TYPE_GYROSCOPE);
+     }
+    
+     protected void onResume() {
+         super.onResume();
+         mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+         mSensorManager.registerListener(this, mGyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+     }
+     protected void onPause() {
+         super.onPause();
+         mSensorManager.unregisterListener(this);
+     }
+     @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        String Name = sensorEvent.sensor.getName();
+        float X=sensorEvent.values[0];
+        float Y=sensorEvent.values[1];
+        float Z=sensorEvent.values[2];
+        Log.d("Debug",": X: " + sensorEvent.values[0] + "; Y: " + sensorEvent.values[1] + "; Z: " + sensorEvent.values[2] + ";");
+        //textView2.setText((int) sensorEvent.values[0]);
+        //textView3.setText((int) sensorEvent.values[1]);
+        //textView4.setText((int) sensorEvent.values[2]);
+    }
 
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
