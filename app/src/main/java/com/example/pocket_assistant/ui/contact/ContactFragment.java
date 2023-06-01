fe71baf2a195c20b5295b40147dbb24a4a18fe0b
tmKla <tm.klajn@gmail.com>
2023-06-01 15:51:59 +0200

    finish

--- app/src/main/java/com/example/pocket_assistant/ui/contact/ContactFragment.java
+++ app/src/main/java/com/example/pocket_assistant/ui/contact/ContactFragment.java
@@ -2,6 +2,7 @@ package com.example.pocket_assistant.ui.contact;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
@@ -16,14 +17,21 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.pocket_assistant.R;
 import com.example.pocket_assistant.databinding.FragmentContactBinding;
 
 public class ContactFragment extends Fragment {
 
     private FragmentContactBinding binding;
+    private final static int NbContactMax=10;
     EditText name_Edit, phone_Edit;
     Button button_Add_Contact;
-    TextView[] name_View= new TextView[10],phone_View= new TextView[10];
+    Button button_Delete_All_Coontact;
+
+    Button[] buttons_delete_Contact =new Button[NbContactMax];
+    TextView[] name_View= new TextView[NbContactMax],phone_View= new TextView[NbContactMax];
+
+    MediaPlayer mediaPlayer;
 
     static int nbContact=0;
 
@@ -41,32 +49,52 @@ public class ContactFragment extends Fragment {
         contactViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
         button_Add_Contact=binding.buttonAddContact;
+        button_Delete_All_Coontact=binding.buttonDeleteAllContact;
         name_Edit=binding.nameEdit;
         phone_Edit= binding.phoneEdit;
         name_View=CreateTabContactName();
         phone_View=CreateTabContactPhone();
+        buttons_delete_Contact=CreateTabButtonDelete();
         context=getActivity();
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
         SharedPreferences.Editor editor = preferences.edit();
         nbContact=preferences.getInt("nbContact",0);
-        for (int i=0; i<10;i++){
+        this.mediaPlayer= MediaPlayer.create(context.getApplicationContext(), R.raw.et_oui_alarme);
+        for (int i=0; i<NbContactMax;i++){
             name_View[i].setText(preferences.getString("name"+i,""));
             phone_View[i].setText(preferences.getString("phone"+i,""));
+            buttons_delete_Contact[i].setVisibility(preferences.getInt("visibility"+i,View.INVISIBLE));
         }
         button_Add_Contact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Add_Contact();
+                play_alarm();
             }
         });
+        button_Delete_All_Coontact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Reset();
+            }
+        });
+        for (int i=0; i<NbContactMax;i++){
+            int finalI = i;
+            buttons_delete_Contact[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Delete_Contact(finalI);
+                }
+            });
+        }
         return root;
     }
     public void Add_Contact(){
         String name= name_Edit.getText().toString();
         String phone= phone_Edit.getText().toString();
-        if(!name.equals("") && !phone.equals("") && nbContact!=10){
+        if(!name.equals("") && !phone.equals("") && nbContact<NbContactMax){
             name_View[nbContact].setText(name);
             phone_View[nbContact].setText(phone);
+            buttons_delete_Contact[nbContact].setVisibility(View.VISIBLE);
             nbContact++;
             name_Edit.setText("");
             phone_Edit.setText("");
@@ -84,35 +112,41 @@ public class ContactFragment extends Fragment {
         if (numContact<nbContact){
             name_View[nbContact-1].setText("");
             phone_View[nbContact-1].setText("");
+            buttons_delete_Contact[nbContact-1].setVisibility(View.INVISIBLE);
             nbContact--;
         }
         saveData();
     }
     public void Reset(){
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        SharedPreferences.Editor editor = preferences.edit();
-        for(int i=0;i<10;i++){
-            editor.putString("name"+i,"");
-            editor.putString("phone"+i,"");
+        for(int i=0;i<NbContactMax;i++){
+            name_View[i].setText("");
+            phone_View[i].setText("");;
+            buttons_delete_Contact[i].setVisibility(View.INVISIBLE);
         }
-        editor.putInt("nbContact",0);
-        editor.apply();
+        nbContact=0;
+        saveData();
     }
     public void saveData(){
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
         SharedPreferences.Editor editor = preferences.edit();
         String name,phone;
-        for(int i=0;i<10;i++){
+        int visibility;
+        for(int i=0;i<NbContactMax;i++){
             name=name_View[i].getText().toString();
             phone=phone_View[i].getText().toString();
+            visibility= buttons_delete_Contact[i].getVisibility();
             editor.putString("name"+i,name);
             editor.putString("phone"+i,phone);
+            editor.putInt("visibility"+i,visibility);
         }
         editor.putInt("nbContact",nbContact);
         editor.apply();
     }
+    public void play_alarm(){
+            mediaPlayer.start();
+    }
         public TextView[] CreateTabContactName(){
-        TextView[] name_View= new TextView[10];
+        TextView[] name_View= new TextView[NbContactMax];
         name_View[0]=binding.nameView0;
         name_View[1]=binding.nameView1;
         name_View[2]=binding.nameView2;
@@ -122,12 +156,12 @@ public class ContactFragment extends Fragment {
         name_View[6]=binding.nameView6;
         name_View[7]=binding.nameView7;
         name_View[8]=binding.nameView8;
-        name_View[9]=binding.nameView8;
+        name_View[9]=binding.nameView9;
         return name_View;
 
     }
     public TextView[] CreateTabContactPhone(){
-        TextView[] phone_View= new TextView[10];
+        TextView[] phone_View= new TextView[NbContactMax];
         phone_View[0]=binding.phoneView0;
         phone_View[1]=binding.phoneView1;
         phone_View[2]=binding.phoneView2;
@@ -140,6 +174,20 @@ public class ContactFragment extends Fragment {
         phone_View[9]=binding.phoneView9;
         return phone_View;
 
+    }
+    public Button[] CreateTabButtonDelete(){
+        Button[] buttons_delete= new Button[NbContactMax];
+        buttons_delete[0]=binding.delelteContact0;
+        buttons_delete[1]=binding.delelteContact1;
+        buttons_delete[2]=binding.delelteContact2;
+        buttons_delete[3]=binding.delelteContact3;
+        buttons_delete[4]=binding.delelteContact4;
+        buttons_delete[5]=binding.delelteContact5;
+        buttons_delete[6]=binding.delelteContact6;
+        buttons_delete[7]=binding.delelteContact7;
+        buttons_delete[8]=binding.delelteContact8;
+        buttons_delete[9]=binding.delelteContact9;
+        return buttons_delete;
     }
 
 
