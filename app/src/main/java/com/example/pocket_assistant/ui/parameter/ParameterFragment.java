c45ecaf1886ad963b2abe002df7c68c6aca52b32
aldesse <Bussiere.alexandre@gmail.com>
2023-06-01 16:31:07 +0200

    lol je suis le merge

--- app/src/main/java/com/example/pocket_assistant/ui/parameter/ParameterFragment.java
+++ app/src/main/java/com/example/pocket_assistant/ui/parameter/ParameterFragment.java
@@ -13,17 +13,12 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.preference.PreferenceManager;
 
 import com.example.pocket_assistant.databinding.FragmentParameterBinding;
 
 public class ParameterFragment extends Fragment {
 
     private FragmentParameterBinding binding;
-    private Button validButton;
-    private EditText message, message2;
-
-    Context context;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -33,26 +28,7 @@ public class ParameterFragment extends Fragment {
         binding = FragmentParameterBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        validButton = binding.valide;
-        message = binding.message;
-        message2 = binding.editTextText;
-        context = getActivity();
 
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        SharedPreferences.Editor editor = preferences.edit();
-
-        validButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-                SharedPreferences.Editor editor = preferences.edit();
-
-                editor.putString("test", message.getText().toString());
-                editor.apply();
-
-                message2.setText(preferences.getString("test42", "NOpe"));
-            }
-        });
 
         return root;
     }
