157cf438717cb32486cd21a3a36e71f064689871
Maelwennlbdr <maelwenn.labidurie@efrei.net>
2023-06-01 12:17:01 +0200

    test first popup

--- app/src/main/java/com/example/pocket_assistant/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/pocket_assistant/ui/home/HomeFragment.java
@@ -1,20 +1,28 @@
 package com.example.pocket_assistant.ui.home;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.pocket_assistant.MainActivity;
 import com.example.pocket_assistant.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    Button button;
+    Context activity;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,6 +31,25 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        button = binding.button;
+        activity = getActivity();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder testPopUp = new AlertDialog.Builder(activity);
+                testPopUp.setTitle("Wesh");
+                testPopUp.setMessage("t'es con");
+                testPopUp.setNegativeButton("Arreter", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(), "t'es pas mort", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                testPopUp.show();
+            }
+        });
 
         return root;
     }
--- app/src/main/java/com/example/pocket_assistant/ui/parameter/ParameterFragment.java
+++ app/src/main/java/com/example/pocket_assistant/ui/parameter/ParameterFragment.java
@@ -1,20 +1,29 @@
 package com.example.pocket_assistant.ui.parameter;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.preference.PreferenceManager;
 
 import com.example.pocket_assistant.databinding.FragmentParameterBinding;
 
 public class ParameterFragment extends Fragment {
 
     private FragmentParameterBinding binding;
+    private Button validButton;
+    private EditText message, message2;
+
+    Context context;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,6 +33,26 @@ public class ParameterFragment extends Fragment {
         binding = FragmentParameterBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        validButton = binding.valide;
+        message = binding.message;
+        message2 = binding.editTextText;
+        context = getActivity();
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        validButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+                SharedPreferences.Editor editor = preferences.edit();
+
+                editor.putString("test", message.getText().toString());
+                editor.apply();
+
+                message2.setText(preferences.getString("test42", "NOpe"));
+            }
+        });
 
         return root;
     }
